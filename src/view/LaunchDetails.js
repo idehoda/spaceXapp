378c2eaf4d039ed009f3dc7ddd0bf6578aff81ab
idehoda <idehoda@gmail.com>
2018-05-04 13:07:31 +0200

    links are now worning

--- src/view/LaunchDetails.js
+++ src/view/LaunchDetails.js
@@ -26,10 +26,10 @@ class LaunchDetails extends Component {
         <footer className="footer">                               
             <ul className="social"> 
                  <li>follow spacex |</li>                      
-                    <li ><a className="social__link" href="">twitter</a></li>
-                    <li ><a className="social__link" href="">youtube</a></li>
-                    <li ><a className="social__link" href="">flickr</a></li>
-                    <li ><a className="social__link" href="">instagram</a></li>
+                    <li ><a className="social__link" href="https://twitter.com/spacex">twitter</a></li>
+                    <li ><a className="social__link" href="https://www.youtube.com/channel/UCtI0Hodo5o5dUb67FeUjDeA">youtube</a></li>
+                    <li ><a className="social__link" href="https://www.flickr.com/photos/spacex/">flickr</a></li>
+                    <li ><a className="social__link" href="https://www.instagram.com/spacex">instagram</a></li>
                 </ul>              
             <div>
                 2018 space exploration technologies corp.
